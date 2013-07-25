b2850d17e06f53a0f03b0a3aa42fd5ca396f8dc1
Mateus <oakcool@yahoo.com>
2013-07-25 20:43:50 -0300

    let do this

--- build.bat
+++ build.bat
@@ -20,3 +20,9 @@ mkdir Build
 .nuget\nuget.exe pack "OakIdeas.AppHarbor.Api\OakIdeas.AppHarbor.Api.csproj" -symbols -o Build -p Configuration=%config% %version%
 copy OakIdeas.AppHarbor.Api\bin\%config%\*.dll Build\
 copy OakIdeas.AppHarbor.Api\bin\%config%\*.pdb Build\
+
+REM Package
+mkdir Build
+.nuget\nuget.exe pack "OakIdeas.AppHarbor.Api.Models\OakIdeas.AppHarbor.Api.Models.csproj" -symbols -o Build -p Configuration=%config% %version%
+copy OakIdeas.AppHarbor.Api.Models\bin\%config%\*.dll Build\
+copy OakIdeas.AppHarbor.Api.Models\bin\%config%\*.pdb Build\
