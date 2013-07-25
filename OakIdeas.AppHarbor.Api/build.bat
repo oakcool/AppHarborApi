bd63caa197a2694731422e0a655b6c3382343e15
Mateus <oakcool@yahoo.com>
2013-07-25 20:31:54 -0300

    ahhhhhh

--- OakIdeas.AppHarbor.Api/build.bat
+++ OakIdeas.AppHarbor.Api/build.bat
@@ -10,13 +10,13 @@ if not "%PackageVersion%" == "" (
 )
 
 REM Package restore
-.nuget\nuget.exe install Microsoft.Net.Http.2.2.13 -OutputDirectory %cd%\packages -NonInteractive
+.nuget\nuget.exe install Microsoft.Net.Http -OutputDirectory %cd%\packages -NonInteractive
 
 REM Build
 %WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild OakIdeas.AppHarbor.Api.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
 
 REM Package
 mkdir Build
-.nuget\nuget.exe pack "OakIdeas.AppHarbor.Api\OakIdeas.AppHarbor.Api.csproj" -symbols -o Build\net40 -p Configuration=%config% %version%
+.nuget\nuget.exe pack "OakIdeas.AppHarbor.Api\OakIdeas.AppHarbor.Api.csproj" -symbols -o Build -p Configuration=%config% %version%
 copy OakIdeas.AppHarbor.Api\bin\%config%\*.dll Build\
 copy OakIdeas.AppHarbor.Api\bin\%config%\*.pdb Build\
