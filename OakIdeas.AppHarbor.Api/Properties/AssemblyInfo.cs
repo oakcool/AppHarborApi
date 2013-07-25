9892422c4da0badbcbdfd2383dad5700c2ff95d5
Mateus <oakcool@yahoo.com>
2013-07-25 14:41:53 -0300

    Added company name

--- OakIdeas.AppHarbor.Api/Properties/AssemblyInfo.cs
+++ OakIdeas.AppHarbor.Api/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("OakIdeas.AppHarbor.Api")]
 [assembly: AssemblyDescription("AppHarbor Api Main Package")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("OakIdeas")]
 [assembly: AssemblyProduct("OakIdeas.AppHarbor.Api")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
