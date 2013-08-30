0862ce2cb9e687cd43bc2e4fd161cffe84b99db4
Mateus <oakcool@yahoo.com>
2013-08-30 16:44:28 -0300

    Added win store solution

---
+++ OakIdeas.AppHarbor.AppStore/Behaviors/ItemClickToCommandBehavior.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace OakIdeas.AppHarbor.AppStore.Behaviors
+{
+    public static class ItemClickToCommandBehavior
+    {
+        public static ICommand GetCommand(DependencyObject obj)
+        {
+            return (ICommand) obj.GetValue(CommandProperty);
+        }
+
+        public static void SetCommand(DependencyObject obj, ICommand value)
+        {
+            obj.SetValue(CommandProperty, value);
+        }
+
+        public static readonly DependencyProperty CommandProperty = DependencyProperty.RegisterAttached("Command",
+                                                                                                        typeof (ICommand
+                                                                                                            ),
+                                                                                                        typeof (
+                                                                                                            ItemClickToCommandBehavior
+                                                                                                            ),
+                                                                                                        new PropertyMetadata
+                                                                                                            (null,
+                                                                                                             OnCommandChanged));
+
+        private static void OnCommandChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            ListViewBase lvb = d as ListViewBase;
+            if (lvb == null)
+            {
+                return;
+            }
+
+            lvb.ItemClick += OnClick;
+        }
+
+        private static void OnClick(object sender, ItemClickEventArgs e)
+        {
+            ListViewBase lvb = sender as ListViewBase;
+            ICommand cmd = lvb.GetValue(ItemClickToCommandBehavior.CommandProperty) as ICommand;
+            if (cmd != null && cmd.CanExecute(e.ClickedItem))
+            {
+                cmd.Execute(e.ClickedItem);
+            }
+        }
+    }
+}
---
+++ OakIdeas.AppHarbor.AppStore/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Practices.Prism.StoreApps;
+using OakIdeas.AppHarbor.Api;
+using OakIdeas.AppHarbor.Api.Models;
+using OakIdeas.AppHarbor.AppStore.Common;
+
+namespace OakIdeas.AppHarbor.AppStore.ViewModels
+{
+    public class MainPageViewModel : ViewModel
+    {
+        private ObservableCollection<ApplicationViewModel> _applications;
+
+        public ObservableCollection<ApplicationViewModel> Applications
+        {
+            get { return _applications ?? (_applications = new ObservableCollection<ApplicationViewModel>()); }
+        }
+
+        public MainPageViewModel()
+        {
+            LoadData();
+        }
+
+        public async void LoadData()
+        {
+            if (String.IsNullOrEmpty(AuthenticationService.Instance.AccessToken))
+            {
+                await AuthenticationService.Instance.CheckAndGetAccessToken();
+            }
+            List<OakIdeas.AppHarbor.Api.Models.Application> applications = await
+                AppHarborApi.Instance.GetApplications(AuthenticationService.Instance.AccessToken);
+
+            foreach (Application application in applications)
+            {
+                _applications.Add(new ApplicationViewModel(application));
+            }
+
+
+
+        }
+    }
+}
